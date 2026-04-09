ab6d07ee5403b7c2b0e49eea1bf2d0db45374611
Artem Proskurko <proskurko@gmail.com>
2026-04-09 21:44:10 +0300

    update: settings optimization

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,16 +2,18 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
-// dotenv.config({ path: `${import.meta.dirname}/.env.test` });
 dotenv.config({ path: path.resolve(process.cwd(), '.env.test') });
 
 export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
-  reporter: "html",
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: process.env.CI ? [["github"], ["html"]] : [["html"]],
   use: {
     baseURL: process.env.BASE_URL || "https://practicesoftwaretesting.com",
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
   },
